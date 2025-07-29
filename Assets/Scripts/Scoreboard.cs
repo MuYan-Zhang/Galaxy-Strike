b9fa6202723c0b6451d9bafd296e8c7cbc10ad18
MuYan-Zhang <muyanz21@gmail.com>
2025-07-28 20:00:32 -0700

    feat:incrementing scoreboard

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
+    [SerializeField] TMP_Text scoreText;
+
     int score = 0;
 
     public void IncrementScore(int amount)
     {
         score += amount;
-        Debug.Log(score);
+        scoreText.text = score.ToString();
     }
 }
